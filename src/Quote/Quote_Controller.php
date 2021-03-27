71688d9d9d7f5419a189c73295f0eb769c88eb83
Glynn <glynn.quelch@gmail.com>
2021-03-27 14:24:42 +0000

    Comments

--- src/Quote/Quote_Controller.php
+++ src/Quote/Quote_Controller.php
@@ -33,11 +33,18 @@ class Quote_Controller implements Registerable {
 	protected $view;
 
 	public function __construct(
-		Quote_Repository $quote_respoitory,
+		Quote_Repository $quote_respoitory, 
 		View $view
 	) {
+		// Even though we are passing an interface here, we can define
+		// which repository to use in the depenedency.php file.
 		$this->quote_respoitory = $quote_respoitory;
-		$this->view             = $view;
+		
+		// View has its own internal depenency (Renderable) which is what is
+		// used to render html. By default we use the PHP engine, but can easily
+		// be done using Blade, Twig, Mustache and any others.
+		// We have a blade module, which can be added really easily if you need.
+		$this->view = $view;
 	}
 
 	/**
@@ -47,6 +54,9 @@ class Quote_Controller implements Registerable {
 	 * @return void
 	 */
 	public function register( Loader $loader ): void {
+		// This will add our filter, BUT it will only be added
+		// When accessing the front end, using admin_filter() will do the reverse
+		// or filter() will be added on either.
 		$loader->front_filter( 'the_content', array( $this, 'render_quote_with_content' ) );
 	}
 
@@ -62,20 +72,27 @@ class Quote_Controller implements Registerable {
 			return $content;
 		}
 
-		// Get a quote from whatever repository
+		// Get a quote from whatever repository we choose to inject.
 		$quote = $this->quote_respoitory->get_quote();
+
+		// If no quote returned, jsut reutrn the inital comment as is.
+		if($quote === null){
+			return $content;
+		}
 
 		// Generate the HTML for the quotes view.
 		// The template can be found
 		$quote_html = $this->view->render(
-			'quote/quote',
-			array(
-				'body'   => $quote->body(),
-				'author' => $quote->author(),
+			'quote/quote', // File is /views/quote/quote.php
+			array( // Sets these as variables for the template
+				'body'   => $quote->body(), // $body
+				'author' => $quote->author(), // $author
 			),
-			View::RETURN_VIEW
+			View::RETURN_VIEW // This ensures the templates html is returned not just printed.
 		);
 
+		// render() can be used to print, so have to check it actually reutrned a string.
+		// If its doenst, just use a blank string.
 		return ( is_string( $quote_html ) ? wp_kses_post( $quote_html ) : '' )
 			. $content;
 	}
